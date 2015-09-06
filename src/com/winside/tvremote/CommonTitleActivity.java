db65e573d750dd43ffce4892233c8fe81c0274d9
lusyoe <lusyoe@163.com>
2015-09-06 18:33:01 +0800

    关于界面添加跳转操作指南，添加滑动返回

--- src/com/winside/tvremote/CommonTitleActivity.java
+++ src/com/winside/tvremote/CommonTitleActivity.java
@@ -73,7 +73,6 @@ public abstract class CommonTitleActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_tutorial, menu);
         return true;
     }
