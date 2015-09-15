5e5f1074dded82b3c594e898e95810308ba47ede
lusyoe <lusyoe@163.com>
2015-09-15 17:30:02 +0800

    添加触摸模式，触点效果

--- src/com/winside/tvremote/widget/SoftDpad.java
+++ src/com/winside/tvremote/widget/SoftDpad.java
@@ -23,8 +23,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.os.Vibrator;
 import android.util.AttributeSet;
+import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.SoundEffectConstants;
 import android.widget.ImageView;
@@ -178,13 +181,39 @@ public final class SoftDpad extends ImageView {
             a.recycle();
         }
 
+        // SparseArray for touch events, indexed by touch id
+        mTouches = new SparseArray<TouchHistory>(10);
+
         initialize();
     }
+    private float mCircleRadius;
+    private float mCircleHistoricalRadius;
+    private Paint mCirclePaint = new Paint();
+
+    // radius of active touch circle in dp default 75f
+    private static final float CIRCLE_RADIUS_DP = 20f;
+    // radius of historical circle in dp default 7f
+    private static final float CIRCLE_HISTORICAL_RADIUS_DP = 7f;
+    public final int[] COLORS = {0xFF33B5E5, 0xFFAA66CC, 0xFF99CC00, 0xFFFFBB33, 0xFFFF4444, 0xFF0099CC, 0xFF9933CC, 0xFF669900, 0xFFFF8800, 0xFFCC0000};
+
+    private SparseArray<TouchHistory> mTouches;
+
+    // Is there an active touch?
+    private boolean mHasTouch = false;
+
 
     private void initialize() {
         isDpadFocused = false;
         setScaleType(ScaleType.CENTER_INSIDE);
         dPadDirection = Direction.IDLE;
+
+        // Calculate radiuses in px from dp based on screen density
+        // 初始化触摸paint
+        float density = getResources().getDisplayMetrics().density;
+        // 手指当前圆的大小
+        mCircleRadius = CIRCLE_RADIUS_DP * density;
+        // 手指之前位置圆的大小
+        mCircleHistoricalRadius = CIRCLE_HISTORICAL_RADIUS_DP * density;
     }
 
     public int getCenterX() {
@@ -236,13 +265,40 @@ public final class SoftDpad extends ImageView {
                     return true;
                 }
                 handleActionDown(x, y);
-                return true;
+
+                int id = event.getPointerId(0);
+
+                LogUtils.e("getX() = " + event.getX() + " getX(0) = " + event.getX(0));
+                TouchHistory data = TouchHistory.obtain(event.getX(0), event.getY(0), event.getPressure(0));
+
+                LogUtils.e("getPressure = " + event.getPressure(0));
+                /*
+                 * Store the data under its pointer identifier. The pointer
+                 * number stays consistent for the duration of a gesture,
+                 * accounting for other pointers going up or down.
+                 */
+                mTouches.put(id, data);
+
+                mHasTouch = true;
+
+               break;
 
             case MotionEvent.ACTION_MOVE:
 //                LogUtils.e("SoftDpad 触发ACTION_MOVE");
                 if (isDpadFocused) {
                     handleActionMove(x, y);
-                    return true;
+
+                    for (int index = 0; index < event.getPointerCount(); index++) {
+                        // get pointer id for data stored at this index
+                        int id_ = event.getPointerId(0);
+                        // get the data stored externally about this pointer.
+                        TouchHistory history = mTouches.get(id_);
+
+                        // add previous position to history and add new values
+                        history.addHistory(history.x, history.y);
+                        history.setTouch(event.getX(index), event.getY(index), event.getPressure(index));
+
+                    }
                 }
                 break;
 
@@ -250,10 +306,18 @@ public final class SoftDpad extends ImageView {
 //                LogUtils.e("SoftDpad 触发ACTION_UP");
                 if (isDpadFocused) {
                     handleActionUp(x, y);
+                    int id_ = event.getPointerId(0);
+                    TouchHistory touchHistory = mTouches.get(id_);
+                    mTouches.remove(0);
+                    touchHistory.recycle();
+
+                    mHasTouch = false;
                 }
                 break;
         }
-        return false;
+        // trigger redraw on UI thread
+        this.postInvalidate();
+        return true;
     }
 
     private void handleActionDown(int x, int y) {
@@ -452,7 +516,114 @@ public final class SoftDpad extends ImageView {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.translate(offsetX, offsetY);
+//        canvas.translate(offsetX, offsetY);
         super.onDraw(canvas);
+
+        int id = 0;
+        // 单点触控
+        if (mTouches.size() > 0) {
+            id = mTouches.keyAt(0);
+            TouchHistory data = mTouches.valueAt(0);
+
+            // draw the data and its history to the canvas
+            drawCircle(canvas, id, data);
+        }
+
+    }
+
+    protected void drawCircle(Canvas canvas, int id, TouchHistory data) {
+        // select the color based on the id
+        int color = COLORS[id % COLORS.length];
+        mCirclePaint.setColor(color);
+
+
+        float pressure = Math.min(data.pressure, 1f);
+        float radius = pressure * mCircleRadius;
+
+        LogUtils.e("data.x = " + data.x + " data.y = " + data.y + " radius = " + radius);
+        canvas.drawCircle(data.x, (data.y) - (radius / 2f), radius, mCirclePaint);
+
+        // draw all historical points with a lower alpha value
+        mCirclePaint.setAlpha(125);
+        for (int j = 0; j < data.history.length && j < data.historyCount; j++) {
+            PointF p = data.history[j];
+            canvas.drawCircle(p.x, p.y, mCircleHistoricalRadius, mCirclePaint);
+        }
+    }
+
+    /**
+     * Holds data related to a touch pointer, including its current position,
+     * pressure and historical positions. Objects are allocated through an
+     * object pool using {@link ()} and {@link #recycle()} to reuse
+     * existing objects.
+     */
+    static final class TouchHistory {
+
+        // number of historical points to store
+        public static final int HISTORY_COUNT = 10;
+
+        public float x;
+        public float y;
+        public float pressure = 0f;
+
+        // current position in history array
+        public int historyIndex = 0;
+        public int historyCount = 0;
+
+        // arrray of pointer position history
+        public PointF[] history = new PointF[HISTORY_COUNT];
+
+        private static final int MAX_POOL_SIZE = 10;
+        private static final Pools.SimplePool<TouchHistory> sPool = new Pools.SimplePool<TouchHistory>(MAX_POOL_SIZE);
+
+        public static TouchHistory obtain(float x, float y, float pressure) {
+            TouchHistory data = sPool.acquire();
+            if (data == null) {
+                data = new TouchHistory();
+            }
+
+            data.setTouch(x, y, pressure);
+
+            return data;
+        }
+
+        public TouchHistory() {
+
+            // initialise history array
+            for (int i = 0; i < HISTORY_COUNT; i++) {
+                history[i] = new PointF();
+            }
+        }
+
+        public void setTouch(float x, float y, float pressure) {
+            this.x = x;
+            this.y = y;
+            this.pressure = pressure;
+        }
+
+        public void recycle() {
+            this.historyIndex = 0;
+            this.historyCount = 0;
+            sPool.release(this);
+        }
+
+        /**
+         * Add a point to its history. Overwrites oldest point if the maximum
+         * number of historical points is already stored.
+         */
+        public void addHistory(float x, float y) {
+            PointF p = history[historyIndex];
+            p.x = x;
+            p.y = y;
+
+            LogUtils.e("x = " + x + " y = " + y);
+
+            historyIndex = (historyIndex + 1) % history.length;
+
+            if (historyCount < HISTORY_COUNT) {
+                historyCount++;
+            }
+        }
+
     }
 }
